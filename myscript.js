b4a270c90ac75b25b0db83a3e249ab7433df022f
roshaun_walker <roshaunwalker@ymail.com>
2022-02-01 10:02:59 -0500

    first commit

---
+++ myscript.js
@@ -0,0 +1,37 @@
+var menuList = document.getElementById("menuList");
+	
+	menuList.style.maxHeight = "0px";
+	function togglemenu(){
+	
+				if(menuList.style.maxHeight == "0px")
+				{
+					menuList.style.maxHeight = "130px";
+				}
+				else{
+					menuList.style.maxHeight = "0px";
+					}
+	}
+
+var slideIndex = [1,1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2","mySlides3","mySlides4","mySlides5","mySlides6"];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
